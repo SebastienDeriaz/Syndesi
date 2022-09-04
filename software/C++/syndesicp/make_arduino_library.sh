fc794b08fe739d332ca5d18e7bc24d12f26bc29c
SebastienDeriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2022-09-04 08:25:41 +0200

    add user_config files to arduino library

--- software/C++/syndesicp/make_arduino_library.sh
+++ software/C++/syndesicp/make_arduino_library.sh
@@ -4,6 +4,7 @@ rm -r syndesicp
 mkdir syndesicp
 mkdir syndesicp/include
 mkdir syndesicp/src
+mkdir syndesicp/user_config
 mkdir syndesicp/examples
 
 touch syndesicp/keywords.txt
@@ -13,5 +14,6 @@ touch syndesicp/examples/test.ino
 
 cp ../include/*.hpp syndesicp
 cp ../src/*.cpp syndesicp
+cp ../user_config/*.hpp syndesicp
 
 zip -r syndesicp.zip syndesicp
